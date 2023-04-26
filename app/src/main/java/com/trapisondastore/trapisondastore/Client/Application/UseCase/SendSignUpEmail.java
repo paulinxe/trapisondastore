acf989a8e99f763af882745e7e0677fdeccb0162
0xa0b0c <abc0xaa@tutanota.com>
2023-04-26 20:34:06 +0200

    sending email. pending to add test

--- app/src/main/java/com/trapisondastore/trapisondastore/Client/Application/UseCase/SendSignUpEmail.java
+++ app/src/main/java/com/trapisondastore/trapisondastore/Client/Application/UseCase/SendSignUpEmail.java
@@ -2,6 +2,7 @@ package com.trapisondastore.trapisondastore.Client.Application.UseCase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.trapisondastore.trapisondastore.Client.Application.Command.SendSignUpEmailCommand;
 import com.trapisondastore.trapisondastore.Shared.Domain.EmailSender;
 
 @Service
@@ -10,7 +11,12 @@ public class SendSignUpEmail {
     @Autowired
     private EmailSender emailSender;
 
-    public void execute() {
-        emailSender.send("test@test.com", "test", "test");
+    public void execute(SendSignUpEmailCommand command) {
+        // @TODO: would be nice to use a html template
+        emailSender.send(
+            command.email,
+            "Welcome to Trapisondastore!",
+            "We are excited to introduce our new ecommerce platform! Our ecommerce platform is designed to provide you with a seamless shopping experience that is both easy to use and secure.\n"
+        );
     }
 }
--- app/src/main/java/com/trapisondastore/trapisondastore/Shared/Infrastructure/Event/Queue/Client/SendEmailOnClientSignedUpQueue.java
+++ app/src/main/java/com/trapisondastore/trapisondastore/Shared/Infrastructure/Event/Queue/Client/SendEmailOnClientSignedUpQueue.java
@@ -1,11 +1,15 @@
 package com.trapisondastore.trapisondastore.Shared.Infrastructure.Event.Queue.Client;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.DeliverCallback;
+import com.trapisondastore.trapisondastore.Client.Application.Command.SendSignUpEmailCommand;
+import com.trapisondastore.trapisondastore.Client.Application.UseCase.SendSignUpEmail;
 import com.trapisondastore.trapisondastore.Shared.Infrastructure.Event.RabbitMQConnection;
 import com.trapisondastore.trapisondastore.Shared.Infrastructure.Event.Queue.RabbitQueue;
 
@@ -13,6 +17,9 @@ import com.trapisondastore.trapisondastore.Shared.Infrastructure.Event.Queue.Rab
 public class SendEmailOnClientSignedUpQueue extends RabbitQueue {
 
     private final static String QUEUE_NAME = "client.client.send_email_on_client_signed_up";
+
+    @Autowired
+    private SendSignUpEmail useCase;
 
     @Autowired
     public SendEmailOnClientSignedUpQueue(RabbitMQConnection connection) throws IOException, TimeoutException {
@@ -26,7 +33,9 @@ public class SendEmailOnClientSignedUpQueue extends RabbitQueue {
             logMessageReceived(SendEmailOnClientSignedUpQueue.class.getName(), QUEUE_NAME);
 
             String message = new String(delivery.getBody(), "UTF-8");
-            logger.info(message);
+            var json = new ObjectMapper().readValue(message, HashMap.class);
+
+            useCase.execute(new SendSignUpEmailCommand(json.get("email").toString()));
         };
         
         pull(QUEUE_NAME, callback);
